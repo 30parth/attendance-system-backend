f298bb972d4cf034408b2ba399613de8837991a7
30parth <aparth3010@gmail.com>
2025-12-11 12:40:31 +0530

    add a method in a cors

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,13 @@ app.use(
   cors({
     origin: APP_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Accept",
+      "X-Requested-With",
+    ],
   })
 );
 
